d5bef84612ea98f58e7a795da0a8d8104479f675
Caroline Katumo <ndukucarole@gmail.com>
2023-11-10 01:20:50 +0300

    minor changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const logger = winston.createLogger({
 
 // Define CORS options
 const corsOptions = {
-  origin: "http://localhost:8080",
+  origin: "http://localhost:4200",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
@@ -26,13 +26,10 @@ const login_text = "Login Request";
 const login_mti = "0100";
 
 const allUsers = [
-  { phone: "254728736229", password: 1234 },
-  { phone: 254710295282, password: 8907 },
-  { phone: 254712979122, password: 6585 },
+  { phone: "254728736229", password: "1234" },
+  { phone: "254710295282", password: "8907" },
+  { phone: "254712979122", password: "6585" },
 ];
-
-// const newPhone = this.phone.replace(/^0|254/g, "");
-// const NP = "254" + newPhone;
 
 app.use(cors(corsOptions));
 
@@ -42,11 +39,12 @@ app.get("/test", (req, res) => {
   res.send("Hello, World!");
 });
 
+const crypto = require("crypto");
+
 app.post("/login", (req, res) => {
   const fieldsToCheck = ["f2", "f37", "f11", "f12", "f13", "f3", "f7", "f56"];
 
   const missingFields = fieldsToCheck.filter((field) => !(field in req.body));
-  console.log(req.body);
 
   if (missingFields.length > 0) {
     res.json({ status_code: 1003, message: "Missing Fields" });
@@ -60,44 +58,53 @@ app.post("/login", (req, res) => {
         ({ phone, password }) =>
           phone === req.body.f2 && password === req.body.f56
       );
-      //   console.log(result);
+
       if (result) {
-        res.json({ status_code: 1000, message: "Successful Login" });
+        const token = crypto.randomBytes(16).toString("hex");
+
+        const tokenExpiration = new Date(Date.now() + 5 * 60 * 1000);
+
+        res.json({
+          status_code: 1000,
+          message: "Successful Login",
+          data: { token, tokenExpiration },
+        });
       } else {
         res.json({ status_code: 1001, message: "Invalid Credentials" });
       }
     }
   }
 });
+
 app.post("/register", (req, res) => {
-    const fieldsToCheck = ["f2", "f37", "f11", "f12", "f13", "f3", "f7", "f56"];
-  
-    const missingFields = fieldsToCheck.filter((field) => !(field in req.body));
-    console.log(req.body);
-  
-    if (missingFields.length > 0) {
-      res.json({ status_code: 1003, message: "Missing Fields" });
+  const fieldsToCheck = ["name", "password", "confirmPassword", "phone"];
+
+  const missingFields = fieldsToCheck.filter((field) => !(field in req.body));
+  console.log(req.body);
+
+  if (missingFields.length > 0) {
+    res.json({ status_code: 1003, message: "Missing Fields" });
+  } else {
+    const result = allUsers.find(({ phone }) => phone === req.body.phone);
+    if (result) {
+      res.json({ status_code: 1002, message: "User Exists. Proceed to Login" });
     } else {
-      if (
-        login_mti === req.body.mti &&
-        login_code === req.body.f3 &&
-        login_text === req.body.f68
-      ) {
-        const result = allUsers.find(
-          ({ phone, password }) =>
-            phone === req.body.f2 && password === req.body.f56
-        );
-        //   console.log(result);
-        if (result) {
-          res.json({ status_code: 1000, message: "Successful Login" });
-        } else {
-          res.json({ status_code: 1001, message: "Invalid Credentials" });
-        }
+      const newUser = req.body;
+      allUsers.push(newUser);
+      console.log("allUsers:", allUsers);
+      if (newUser !== undefined) {
+        res.json({
+          status_code: 1000,
+          message: "Successful Registration. Proceed to Login",
+        });
+      } else {
+        res.json({ status_code: 1001, message: "Unsuccessful Registration" });
       }
     }
-  });
+  }
+});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
